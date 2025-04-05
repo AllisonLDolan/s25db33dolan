c93bcf0f2c5598a99fb4a49f6971d99e6afa3ecc
AllisonLDolan <145593596+AllisonLDolan@users.noreply.github.com>
2025-04-05 12:52:50 -0500

    maybe fix

--- routes/creature.js
+++ routes/creature.js
@@ -6,14 +6,5 @@ router.get('/', function(req, res, next) {
   res.render('creature', { title: 'Search Results' });
 });
 
-const mongoose = require("mongoose")
-const creatureSchema = mongoose.Schema({
-  creature: String,
-  habitat: String,
-  lifespan: Number
-})
-module.exports = mongoose.model("Creature",
-  creatureSchema)
-
 module.exports = router;
 
