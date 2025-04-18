e080b8ae0ed6f5e480b30d92d37ff5c1b2a9f131
AllisonLDolan <145593596+AllisonLDolan@users.noreply.github.com>
2025-04-18 12:21:39 -0500

    part 5

--- models/creature.js
+++ models/creature.js
@@ -1,7 +1,21 @@
 const mongoose = require("mongoose")
 const creatureSchema = mongoose.Schema({
-  creature: String,
-  habitat: String,
-  lifespan: Number
+  creature: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50
+  },
+  habitat: {
+    type: String,
+    required: true,
+    enum: ['Forest', 'Ocean', 'Desert', 'Grassland', 'Mountains', 'Urban']
+  },
+  lifespan: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 5000
+  }
 })
 module.exports = mongoose.model("Creature", creatureSchema)
